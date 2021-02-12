995b9ea85b6d4e0482eaf21a8cfa1a2182cbaf71
mihanchik94 <mihanchik94@gmail.com>
2021-02-12 20:33:34 +0300

    Trigger codecov

---
+++ src/test/java/ru/job4j/TriggerTest.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriggerTest {
+
+    @Test
+    public void someLogic() {
+        assertEquals(1, new Trigger().someLogic());
+    }
+}
