983627fd62564ab09b13fc129942d581745773e4
mihanchik94 <mihanchik94@gmail.com>
2021-02-19 09:56:52 +0300

    Ключевые варианты реализации generics: тип 1 - WildCard

--- src/main/java/ru/job4j/generics/GenericUsage.java
+++ src/main/java/ru/job4j/generics/GenericUsage.java
@@ -1,8 +1,6 @@
 package ru.job4j.generics;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * Generics в данном случае это запись, заключенная в скобки <>, т.е. <String>.
@@ -21,6 +19,28 @@ public class GenericUsage {
         String i = (String) list.get(1); // команда .get выводит элементы класса Object, поэтому для избежания ошибки компиляции, необходимо произвести приведение (down casting) к лассу String
         for (String s: list) {
             System.out.println("Текущий элемент " + s);
+        }
+
+        //пример реализации метода printRsl():
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        new GenericUsage().printRsl(numbers);
+
+    }
+
+    /**
+     *В версии 1.5 были добавлены средства обобщенного программирования.
+     *Добавим универсальный метод, который будет выводить в консоль наш список.
+     *Так как Collection<Object> не является полностью родительской коллекцией всех остальных коллекций, (т.е. имеет ограничения - например, в методе main() для типа данных в списке Integer мы получим ошибку компиляции),
+     *Вместо public void printRsl(Collection<Object> col) {...
+     *Мы используем так называемый WildCard: public void printRsl(Collection<?> col) {...
+     *В этом случае ограничений в использовании не будет (т.е. он имеет соответствие с любым типом).
+     */
+
+    public void printRsl(Collection<?> col) {
+        System.out.println("Реализация метода printRsl:");
+        for (Iterator<?> it = col.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
         }
     }
 }
