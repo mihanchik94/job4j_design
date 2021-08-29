d2340dd87186d9edfe7a0b8aa86052aaaf44593a
mihanchik94 <mihanchik94@gmail.com>
2021-08-29 18:16:28 +0300

    1. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection.map;
+
+/*
+Создать модель User и три поля String name, int children, Calendar birthday.
+Поля должны быть приватными. Добавьте конструктор.
+ */
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}
