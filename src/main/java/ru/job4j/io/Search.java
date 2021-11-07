6f312b2a9399cc2eace80edb607a1856fc4d5f4c
mihanchik94 <mihanchik94@gmail.com>
2021-11-07 14:32:08 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,9 +8,23 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+    public static boolean validate(String[] args) {
+        Path path = Path.of(args[0]);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Specify the source folder and the desired extension!");
+        }
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Directory is not exist or is not a directory!");
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().contains("Test")).forEach(System.out::println);
+        if (!Search.validate(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
