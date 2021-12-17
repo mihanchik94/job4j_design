07ca181d5f8c8692cf11bb64cd3b0e54b61e350c
mihanchik94 <mihanchik94@gmail.com>
2021-12-17 14:28:56 +0300

    2. Поиск файлов по критерию [#783] (r.1.01)

--- src/main/java/ru/job4j/io/exam/FileSearcher.java
+++ src/main/java/ru/job4j/io/exam/FileSearcher.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,19 +19,47 @@ public class FileSearcher {
     private static final Pattern PATH_FOLDER = Pattern.compile("([A-Z|a-z]:\\\\[^*|\"\\n]*)");
     private static final Pattern FILE_NAME = Pattern.compile("\\w+");
     private static final Pattern FILE_TYPE = Pattern.compile("[a-z]");
-    private static final Pattern SEARCH_RESULT = Pattern.compile("[a-zA-Z0-9\\\\.a-z]");
     private static List<Path> result = new ArrayList<>();
+    private static Predicate<Path> predicate;
 
+
+    private static Predicate<Path> getPredicate(ArgsName args) {
+        if (Objects.equals(args.get("t"), "name")) {
+            predicate = path -> path.toFile().getName().equals(args.get("n"));
+        }
+        if (Objects.equals(args.get("t"), "mask")) {
+            predicate = foundRegex(getMask(args));
+        }
+        return predicate;
+    }
+
+    private static String getMask(ArgsName args) {
+        String mask = args.get("n");
+        if (mask.contains("*.")) {
+            mask = mask.replace("*", "^.*") + "$";
+        } else {
+            if (mask.contains("?.")) {
+                mask = mask.replace("?", ".*\\") + "$";
+            } else {
+                throw new IllegalArgumentException("Mask is wrong. As an example to find all files which start on \"res\" you need to use \"res?.txt\""
+                        + " And if you want to find all files of \"txt\" format you need to use \"*.txt\"");
+            }
+        }
+        return mask;
+    }
+
+    private static Predicate<Path> foundRegex(String file) {
+        return foundRegex -> {
+            Pattern pattern = Pattern.compile(file);
+            Matcher matcher = pattern.matcher(foundRegex.toFile().getName());
+            return matcher.find();
+        };
+    }
 
     public static void handle(ArgsName args) throws IOException {
         validate(args);
         Path startFolder = Paths.get(args.get("d"));
-        if (Objects.equals(args.get("t"), "name")) {
-            result = Search.search(startFolder, path -> path.toFile().getName().equals(args.get("n")));
-        }
-        if (Objects.equals(args.get("t"), "mask")) {
-            result = Search.search(startFolder, path -> path.toFile().getName().endsWith(args.get("n").substring(1)));
-        }
+        result = Search.search(startFolder, getPredicate(args));
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(args.get("o")))) {
             for (Path file : result) {
                 writer.write(file.toString());
@@ -55,11 +84,6 @@ public class FileSearcher {
         Matcher searchTypeMatcher = FILE_TYPE.matcher(args.get("t"));
         if (!searchTypeMatcher.find()) {
             throw new IllegalArgumentException("Search type specified incorrectly");
-        }
-        Matcher searchResultMatcher = SEARCH_RESULT.matcher(args.get("o"));
-        Path resultFile = Paths.get(args.get("o"));
-        if (!resultFile.toFile().exists() && searchResultMatcher.find()) {
-            throw new IllegalArgumentException("The file is specified incorrectly");
         }
     }
 
