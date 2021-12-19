861ce3ddf9b8a678c94b24b27ba34d97a1094f13
mihanchik94 <65723214+mihanchik94@users.noreply.github.com>
2021-12-20 00:41:45 +0300

    0. Что такое база данных. [#6860] (r.1.02)

--- all.sql
+++ all.sql
@@ -1,1 +1,14 @@
-select * from clients;
+create table clients(
+id serial primary key,
+name varchar (255),
+age smallint,
+email text);
+
+insert into clients(name, age, email) values('Igor', '21', 'igo@rambler.ru');
+
+update clients set email = 'igor@rambler.ru';
+
+delete from clients;
+
+select* from clients;
+
