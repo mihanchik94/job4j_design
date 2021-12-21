0a92a6f2353ef11bad51335abf0ab752fac7cebb
mihanchik94 <65723214+mihanchik94@users.noreply.github.com>
2021-12-21 22:14:29 +0300

    1. Схема прав пользователи и роли. [#1093]

--- create.sql
+++ create.sql
@@ -1,5 +1,42 @@
-create table clients (
+create table role(
 id serial primary key,
-name varchar (255),
-age smallint,
-email text)
+role_name text);
+
+create table users(
+id serial primary key,
+name varchar(255),
+role_id int references role(id));
+
+create table rules(
+id serial primary key,
+rule_name text);
+
+create table role_rules(
+id serial primary key,
+role_id int references role(id),
+rules_id int references rules(id));
+
+create table category(
+id serial primary key,
+category_name text);
+
+create table state(
+id serial primary key,
+state_name text);
+
+create table item(
+id serial primary key,
+name varchar(255),
+user_id int references users(id),
+category_id int references category(id),
+state_id int references state(id));
+
+create table attachs(
+id serial primary key,
+name_attach text,
+item_id int references item(id));
+
+create table comments(
+id serial primary key,
+comments_name text,
+item_id int references item(id));
---
+++ init.sql
@@ -0,0 +1,12 @@
+-- Database:  User rights and roles schema
+
+-- DROP DATABASE IF EXISTS " User rights and roles schema";
+
+CREATE DATABASE " User rights and roles schema"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
